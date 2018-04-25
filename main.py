bc4dc2cc4a1825e2f56b55e96540563a68204443
Jayde Mitchell <jdmitchell@alumni.berklee.edu>
2018-04-24 23:04:53 -0500

    progress

--- main.py
+++ main.py
@@ -2,19 +2,33 @@ from flask import Flask, request, redirect, render_template, session, flash
 from flask_sqlalchemy import SQLAlchemy 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:password@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:password@localhost:8889/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 app.secret_key = 'password'
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120))
+    password = db.Column(db.String(120))
+    blogs = db.relationship('Blog', backref='author')
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+    
 
 class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
     body = db.Column(db.String(2000))
+    author_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-    def __init__(self, title, body):
+    def __init__(self, title, body, author):
         self.title = title
         self.body = body
+        self.author = author
+
   
 
 @app.route('/', methods=['POST', 'GET'])
@@ -35,6 +49,7 @@ def index():
     else:
         title_error = ''
         body_error = ''
+        authorchangethis = 'change this' #change this and in the Blog call below
         title_name = request.form['title']
         body_content = request.form['body']
         if title_name == '':
@@ -45,7 +60,7 @@ def index():
         rules = (body_error == '', title_error == '')
 
         if all(rules):
-            new_blog = Blog(title_name, body_content)
+            new_blog = Blog(title_name, body_content, authorchangethis)
             db.session.add(new_blog)
             db.session.commit()
             blogposts = Blog.query.all()
@@ -60,7 +75,39 @@ def newpost():
     
     return render_template('newpost.html')
     
+@app.route('/login', methods = ['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user and user.password == password:
+            return redirect("/")
+            #remember user
+        else:
+            #tell them why it failed
+            return '<h1>Error: No user exists</h1>'
+    return render_template("login.html")
 
+@app.route('/signup', methods = ['POST', 'GET'])
+def signup():
+    if request.method == "POST":
+        username = request.form['username']
+        password = request.form['password']
+        confirmpassword = request.form['confirmpassword']
+
+        #need to verify the passwords match. Check user signup assignment.
+
+        existing_user = User.query.filter_by(username=username).first()
+        if not existing_user:
+            new_user = User(username, password)
+            db.session.add(new_user)
+            db.session.commit()
+        else: 
+            #return message that they already exist
+            return '<h1>Duplicate User</h1>'
+
+    return render_template('signup.html')
 
 if __name__ == '__main__':
     app.run()
