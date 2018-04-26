a0dc7b6753bba51e1d2afff8f6eb40720ca6bc82
Jayde Mitchell <jdmitchell@alumni.berklee.edu>
2018-04-25 20:10:25 -0500

    finished the early steps

--- main.py
+++ main.py
@@ -29,7 +29,13 @@ class Blog(db.Model):
         self.body = body
         self.author = author
 
-  
+@app.before_request
+def require_login():
+    allowed_routes = ['login', 'signup', 'index']
+    if request.endpoint not in allowed_routes and 'username' not in session:
+        return redirect('/login')
+
+
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
@@ -49,7 +55,11 @@ def index():
     else:
         title_error = ''
         body_error = ''
-        authorchangethis = 'change this' #change this and in the Blog call below
+        author = session['username'] #change this and in the Blog call below
+        password = session['password']
+        author_id = User.query.filter_by(username=author).first()
+        print(author_id)
+        print('HAAAA')
         title_name = request.form['title']
         body_content = request.form['body']
         if title_name == '':
@@ -60,7 +70,7 @@ def index():
         rules = (body_error == '', title_error == '')
 
         if all(rules):
-            new_blog = Blog(title_name, body_content, authorchangethis)
+            new_blog = Blog(title_name, body_content, author_id)
             db.session.add(new_blog)
             db.session.commit()
             blogposts = Blog.query.all()
@@ -82,11 +92,14 @@ def login():
         password = request.form['password']
         user = User.query.filter_by(username=username).first()
         if user and user.password == password:
+            session['username'] = username
+            session['password'] = password
+            flash("Logged In")
             return redirect("/")
-            #remember user
         else:
-            #tell them why it failed
-            return '<h1>Error: No user exists</h1>'
+            flash('User password incorrect, or user does not exist', 'error')
+            
+
     return render_template("login.html")
 
 @app.route('/signup', methods = ['POST', 'GET'])
@@ -115,10 +128,11 @@ def signup():
         if password != confirmpassword:
             confirm_error = "Your passwords don't match"
         existing_user = User.query.filter_by(username=username).first()
+        if not existing_user == True:
+            existing_error = 'This user already exists'
         if not existing_user:
             existing_error = ''
-        if not existing_user == False:
-            existing_error = 'This user already exists'
+
         
         rules = (username_error == "", password_error == "", 
         confirm_error == "", existing_error == '')
@@ -126,25 +140,21 @@ def signup():
             new_user = User(username, password)
             db.session.add(new_user)
             db.session.commit()
-            return redirect('/')
+            session['username'] = username
+            return redirect('/newpost')
         
         return render_template('signup.html', username_error=username_error, 
         password_error=password_error, confirm_error=confirm_error,
         username=username, existing_error=existing_error)
         #need to verify the passwords match. Check user signup assignment.
 
-        existing_user = User.query.filter_by(username=username).first()
-        if not existing_user:
-            new_user = User(username, password)
-            db.session.add(new_user)
-            db.session.commit()
-            # TODO - remember this user
-            return redirect ('/')
-        else: 
-            #return message that they already exist
-            return '<h1>Duplicate User</h1>'
 
     return render_template('signup.html')
+
+@app.route('/logout')
+def logout():
+    del session['username']
+    return redirect('/login')
 
 if __name__ == '__main__':
     app.run()
